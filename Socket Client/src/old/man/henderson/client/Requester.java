2cad7ae45ff1fbf1914ae4f2dd754326d5c6490d
Leon Hodges-Austin <Lhodge6@students.towson.edu>
2012-09-20 13:05:04 -0400

    clients and servers

---
+++ Socket Client/src/old/man/henderson/client/Requester.java
@@ -0,0 +1,71 @@
+package old.man.henderson.client;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+public class Requester{
+	Socket requestSocket;
+	ObjectOutputStream out;
+ 	ObjectInputStream in;
+ 	String message;
+	Requester(){}
+	Scanner sc = new Scanner(System.in);
+	void run()
+	{
+		try{
+			//1. creating a socket to connect to the server
+			requestSocket = new Socket("10.128.165.61", 2004);
+			System.out.println("Connected to localhost in port 2004");
+			//2. get Input and Output streams
+			out = new ObjectOutputStream(requestSocket.getOutputStream());
+			out.flush();
+			in = new ObjectInputStream(requestSocket.getInputStream());
+			//3: Communicating with the server
+			do{
+				try{
+					message = (String)in.readObject();
+					System.out.println("server>" + message);
+					//this takes sql queries, will crash if not absolutely perfect, should reboot...
+					sendMessage(sc.nextLine());
+				}
+				catch(ClassNotFoundException classNot){
+					System.err.println("data received in unknown format");
+				}
+			}while(!message.equals("bye"));
+		}
+		catch(UnknownHostException unknownHost){
+			System.err.println("You are trying to connect to an unknown host!");
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+		finally{
+			//4: Closing connection
+			try{
+				in.close();
+				out.close();
+				requestSocket.close();
+			}
+			catch(IOException ioException){
+				ioException.printStackTrace();
+			}
+		}
+	}
+	void sendMessage(String msg)
+	{
+		try{
+			out.writeObject(msg);
+			out.flush();
+			System.out.println("client>" + msg);
+		}
+		catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+	}
+	public static void main(String args[])
+	{
+		Requester client = new Requester();
+		while(true){
+			client.run();
+		}
+	}
+}
