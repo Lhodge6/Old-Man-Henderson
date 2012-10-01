fc87156e675cadccdef2fc9cce78cebcc2066902
Leon Hodges-Austin <Lhodge6@students.towson.edu>
2012-10-01 16:10:25 -0400

    set the client to spam the server for load testing

--- Socket Client/src/old/man/henderson/client/Requester.java
+++ Socket Client/src/old/man/henderson/client/Requester.java
@@ -4,50 +4,64 @@ import java.net.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
-public class Requester{
+public class Requester implements Runnable{
 	Socket requestSocket;
 	ObjectOutputStream out;
  	ObjectInputStream in;
  	String message;
 	Requester(){}
 	Scanner sc = new Scanner(System.in);
-	void run()
+	public void run()
 	{
 		try{
 			//1. creating a socket to connect to the server
-			requestSocket = new Socket("192.168.1.30", 2005);
+			requestSocket = new Socket("192.168.1.1", 2008);
 			System.out.println("Connected to " + requestSocket.getInetAddress().getHostName() + " in port " + requestSocket.getPort());
 			
-			//2. get Input stream			
-			out = new ObjectOutputStream(requestSocket.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(requestSocket.getInputStream());
-			
-			int newPort = Integer.parseInt(in.readObject().toString());
-			System.out.println("New Port number : " + newPort);
-			System.out.println("Connecting...");
-			requestSocket.close();
-			
-			//3. get server port from negotiator
-			requestSocket = new Socket("192.168.1.30", newPort);
-			System.out.println("Connected to " + requestSocket.getInetAddress().getHostName() + " in port " + requestSocket.getPort());
-			
-			out = new ObjectOutputStream(requestSocket.getOutputStream());
-			out.flush();
-			in = new ObjectInputStream(requestSocket.getInputStream());
-			//3: Communicating with the server
+			boolean exception = false;
 			do{
-				try{
-					message = (String)in.readObject();
-					System.out.println("server>" + message);
+				
+					//2. get Input stream			
+					out = new ObjectOutputStream(requestSocket.getOutputStream());
+					out.flush();
+					in = new ObjectInputStream(requestSocket.getInputStream());
 					
-					//this takes sql queries, will crash if not absolutely perfect, should reboot...
-					sendMessage(sc.nextLine());
-				}
-				catch(ClassNotFoundException classNot){
-					System.err.println("data received in unknown format");
-				}
-			}while(!message.equals("bye"));
+					int newPort = Integer.parseInt(in.readObject().toString());
+					System.out.println("New Port number : " + newPort);
+					System.out.println("Connecting...");
+					requestSocket.close();
+					
+					//3. get server port from negotiator
+					requestSocket = new Socket("192.168.1.30", newPort);
+					System.out.println("Connected to " + requestSocket.getInetAddress().getHostName() + " in port " + requestSocket.getPort());
+					
+					out = new ObjectOutputStream(requestSocket.getOutputStream());
+					out.flush();
+					in = new ObjectInputStream(requestSocket.getInputStream());
+					//3: Communicating with the server
+					do{
+						try{
+							while(true){
+								message = (String)in.readObject();
+								System.out.println("server>" + message);
+								
+								//this takes sql queries, will crash if not absolutely perfect, should reboot...
+								//sendMessage(sc.nextLine());
+							
+								sendMessage("select * from USERS;");
+								Thread.sleep((long) (Math.random() * 5000l));
+							}
+						}
+						catch(ClassNotFoundException classNot){
+							System.err.println("data received in unknown format");
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}while(!message.equals("bye"));
+				
+				
+			}while(exception);
 		}
 		catch(UnknownHostException unknownHost){
 			System.err.println("You are trying to connect to an unknown host!");
@@ -86,9 +100,16 @@ public class Requester{
 	}	
 	public static void main(String args[])
 	{
-		Requester client = new Requester();
 		while(true){
-			client.run();
+			Requester client = new Requester();
+			Thread temp = new Thread(client);
+			temp.start();
+			try {
+				Thread.sleep(1000L);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
